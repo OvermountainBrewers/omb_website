9b350ca601d547a2eed8796a5078d9047b8ce9b0
Jonah Walker <supposedlysam@gmail.com>
2024-06-22 12:48:47 -0400

    feat: use next/link

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useWindowSize } from "./hooks/window_size";
+import Link from "next/link";
 
 function anchor({
   href,
@@ -12,7 +13,7 @@ function anchor({
   description: string;
 }): JSX.Element {
   return (
-    <a
+    <Link
       href={href}
       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
     >
@@ -23,7 +24,7 @@ function anchor({
         </span>
       </h2>
       <p className="m-0 max-w-[30ch] text-sm opacity-50">{description}</p>
-    </a>
+    </Link>
   );
 }
 
