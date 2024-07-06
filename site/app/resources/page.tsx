6afa42deed7f813ce212382acb365d262f9959dc
Jonah Walker <supposedlysam@gmail.com>
2024-07-06 13:19:34 -0400

    feat: style resources page

--- site/app/resources/page.tsx
+++ site/app/resources/page.tsx
@@ -1,5 +1,9 @@
 import { Metadata } from "next";
 import { getResources } from "@/lib/sanity/sanity.endpoints";
+import { cardStyle } from "@/components/card";
+import { H1, H2, P } from "@/components/typography";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: {
@@ -12,15 +16,17 @@ export default async function Page() {
   const resources = await getResources();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Resources</h1>
-      <section>
+    <main className="flex flex-col p-24">
+      <H1>Resources</H1>
+      <section id="downloads">
         <ul>
           {resources.map((resource) => (
-            <li key={resource._id}>
-              <h2>{resource.name}</h2>
-              <p>{resource.description}</p>
-              <a href={`${resource.fileUrl}?dl=`}>Download</a>
+            <li key={resource._id} className={cardStyle}>
+              <H2>{resource.name}</H2>
+              <P>{resource.description}</P>
+              <Button asChild className="mt-12">
+                <Link href={`${resource.fileUrl}?dl=`}>Download</Link>
+              </Button>
             </li>
           ))}
         </ul>
