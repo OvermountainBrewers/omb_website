a13da1ff6479919edc227d654b787ea956dd4aa9
Jonah Walker <supposedlysam@gmail.com>
2024-12-27 21:55:43 -0500

    refactor: use same style for galleryImage.ts

--- cms/schemaTypes/galleryImage.ts
+++ cms/schemaTypes/galleryImage.ts
@@ -1,46 +1,52 @@
-export const galleryImage = {
+import { ImageIcon as icon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+import { required } from "../validations";
+
+export const galleryImageType = defineType({
     name: 'galleryImage',
     title: 'Gallery Image',
     type: 'document',
+    icon: icon,
     fields: [
-        {
+        defineField({
             name: 'title',
             title: 'Title',
             type: 'string',
-            validation: (Rule: any) => Rule.required(),
-        },
-        {
+            validation: required,
+        }),
+        defineField({
             name: 'event',
             title: 'Event Name',
             type: 'string',
-        },
-        {
+            validation: required,
+        }),
+        defineField({
             name: 'image',
             title: 'Image',
             type: 'image',
             options: {
                 hotspot: true,
             },
-            validation: (Rule: any) => Rule.required(),
-        },
-        {
+            validation: required,
+        }),
+        defineField({
             name: 'date',
             title: 'Date',
             type: 'datetime',
-            validation: (Rule: any) => Rule.required(),
-        },
-        {
+            validation: required,
+        }),
+        defineField({
             name: 'alt',
             title: 'Alt Text',
             type: 'string',
-            validation: (Rule: any) => Rule.required(),
-        },
-        {
+            validation: required,
+        }),
+        defineField({
             name: 'sourceFolder',
             title: 'Source Folder ID',
             type: 'string',
             hidden: true, // This will help us track which Drive folder the image came from
-        }
+        }),
     ],
     preview: {
         select: {
@@ -48,5 +54,5 @@ export const galleryImage = {
             subtitle: 'event',
             media: 'image'
         }
-    }
-} 
+    },
+});
